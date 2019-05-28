6ca2fe13cf38560273e4ffabbd6a31a7ad1d68c0
martbogdan <mbogdan0123@gmail.com>
2019-05-28 11:09:51 +0300

    Task 2 added count

--- Main2.java
+++ Main2.java
@@ -2,13 +2,16 @@ public class Main2 {
     public static void main (String[] args){
 
         String str = "abcdef.ghi";
+        int count =0;
 
         for (int i=0; i<str.length(); i++){
             if (str.charAt(i)=='.'){
                 break;
             }
-                System.out.println(str.charAt(i));
+            count++;
+            System.out.println(str.charAt(i));
             }
+        System.out.println("number of characters is "+count);
         }
     }
 
